8662027df4f7bb36f593afc44dbe004607b6f86c
Manoj <developertestnew@gmail.com>
2024-06-28 19:18:35 +0530

    move

--- web/index.php
+++ web/index.php
@@ -1,39 +1,17 @@
 <?php
-die('1');
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Log\LoggerInterface;
-use DI\Container;
-use DI\Bridge\Slim\Bridge;
-use Monolog\Handler\StreamHandler;
-use Monolog\Level;
-use Monolog\Logger;
-use Slim\Factory\AppFactory;
-use Slim\Views\Twig;
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-require(__DIR__.'/../vendor/autoload.php');
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-// Create DI container
-$container = new Container();
-// Add Twig to Container
-$container->set(Twig::class, function() {
-  return Twig::create(__DIR__.'/../views');
-});
-// Add Monolog to Container
-$container->set(LoggerInterface::class, function () {
-  $logger = new Logger('default');
-  $logger->pushHandler(new StreamHandler('php://stderr'), Level::Debug);
-  return $logger;
-});
-
-// Create main Slim app
-$app = Bridge::create($container);
-$app->addErrorMiddleware(true, false, false);
-
-// Our web handlers
-$app->get('/', function(Request $request, Response $response, LoggerInterface $logger, Twig $twig) {
-  $logger->debug('logging output.');
-  return $twig->render($response, 'index.twig');
-});
-
-$app->run();
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
